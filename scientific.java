3972fffb48c1ffd1b113eb162f4e3065a2edfe31
sriswetha-21CSE089 <111385840+sriswetha-21CSE089@users.noreply.github.com>
2022-09-17 21:38:10 +0530

    Add files via upload

---
+++ scientific.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pack;
+
+/**
+ *
+ * @author Vishnu
+ */
+public class scientific 
+{
+ public void cosine(double a)
+    {
+        System.out.println(Math.cos(a));
+    }
+    public void tan(double a)
+    {
+        System.out.println(Math.atan(a));
+    }
+    public void expo(double a)
+    {
+        System.out.println(Math.exp(a));
+    }
+    public void ceil(double a)
+    {
+        System.out.println(Math.ceil(a));
+    }
+    public void sine(double a)
+    {
+        System.out.println(Math.asin(a));
+    }
+    public void abrt(double a)
+    {
+        System.out.println(Math.abs(a));
+    }
+}
